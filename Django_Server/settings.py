9efb80dc308ab9f113be5da6ed3a62a31aab488e
kahyunnam <69875166+kahyunnam@users.noreply.github.com>
2021-11-04 19:22:02 -0400

    disable collectstatic

--- Django_Server/settings.py
+++ Django_Server/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+# herok config settings N_O_T_E@kahyun: remove later maybe
+DISABLE_COLLECTSTATIC=1
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
