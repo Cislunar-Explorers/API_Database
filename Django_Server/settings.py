8706b7be1b0b703c494b0e20bcc5cb1d392f9efa
kahyunnam <69875166+kahyunnam@users.noreply.github.com>
2021-11-11 15:49:00 -0500

    cors fix?

--- Django_Server/settings.py
+++ Django_Server/settings.py
@@ -60,6 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # CORS fix 
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'Django_Server.urls'
